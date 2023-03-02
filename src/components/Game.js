e13e8b16b066fe4a3579f66d24852b389fe6d7d2
Vidiic <jordividal1997@gmail.com>
2023-03-02 23:56:02 +0100

    modal choose player

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,11 +1,27 @@
 import { useState } from "react";
 import Board from "./Board";
 import History from "./History";
+import Modal from 'react-modal';
+
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        padding: 0,
+        borderRadius: '20px',
+        background: '#23272fde',
+        transform: 'translate(-50%, -50%)',
+    },
+};
 
 export default function Game() {
     const [history, setHistory] = useState([Array(9).fill(null)]);
     const [currentMove, setCurrentMove] = useState(0);
     const currentSquares = history[currentMove];
+    const [modalIsOpen, setIsOpen] = useState(true);
     const xIsNext = currentMove % 2 === 0;
 
     function handlePlay(nextSquares) {
@@ -21,6 +37,15 @@ export default function Game() {
     function handleRestart() {
         setHistory([Array(9).fill(null)]);
         setCurrentMove(0);
+        openModal();
+    }
+
+    function openModal() {
+        setIsOpen(true);
+    }
+
+    function closeModal() {
+        setIsOpen(false);
     }
 
     return (
@@ -34,6 +59,19 @@ export default function Game() {
             <div className="game-info">
                 <History history={history} currentMove={currentMove} onHistoryClick={jumpTo} onRestartClick={handleRestart} />
             </div>
+            <Modal
+                isOpen={modalIsOpen}
+                style={customStyles}
+                contentLabel="Player Modal"
+            >
+                <div className="game-modal">
+                    <span>Choose your player:</span>
+                    <div className="cards">
+                        <div onClick={closeModal}>X</div>
+                        <div onClick={closeModal}>O</div>
+                    </div>
+                </div>
+            </Modal>
         </div>
     );
 }
